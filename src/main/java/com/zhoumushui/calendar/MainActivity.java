8292e5abda73e12df773c51eb723e25a6251ec03
zhoumushui <zhoumushui@gmail.com>
2017-04-06 17:37:21 +0800

    [ZMS]Using view pager, ready for deprecated.

--- src/main/java/com/zhoumushui/calendar/MainActivity.java
+++ src/main/java/com/zhoumushui/calendar/MainActivity.java
@@ -3,13 +3,23 @@ package com.zhoumushui.calendar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhoumushui.calendar.util.MyLog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private List<View> viewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,23 +58,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupViewPager() {
+        viewList = new ArrayList<>();
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        viewList.add(layoutInflater.inflate(R.layout.activity_calendar, null));
+        viewList.add(layoutInflater.inflate(R.layout.activity_past, null));
+        viewList.add(layoutInflater.inflate(R.layout.activity_chronograph, null));
+
         viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(pagerAdapter);
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+                MyLog.d("onPageScrolled:" + position);
             }
 
             @Override
             public void onPageSelected(int position) {
-
+                MyLog.d("onPageSelected:" + position);
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
 
+                MyLog.d("onPageScrollStateChanged:" + state);
             }
         });
+    }
+
+    private PagerAdapter pagerAdapter = new PagerAdapter() {
+        @Override
+        public int getCount() {
+            return viewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return super.getItemPosition(object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(viewList.get(position));
+            updateLayout(position);
+
+            return viewList.get(position);
+        }
+    };
+
+    private void updateLayout(int position) {
+        MyLog.v("updateLayout:" + position);
     }
 
 
--- src/main/java/com/zhoumushui/calendar/util/MyLog.java
+++ src/main/java/com/zhoumushui/calendar/util/MyLog.java
@@ -1,63 +1,62 @@
 package com.zhoumushui.calendar.util;
 
-import com.tchip.autorecord.Constant;
-
 import android.util.Log;
 
 public class MyLog {
 
-	public static final String TAG_DEFAULT = "AZ";
+    private static boolean isDebug = true;
+    private static final String TAG_DEFAULT = "ZMS";
 
-	private static String MODULE = "[AutoRecord]";
+    private static String MODULE = "[Calendar]";
 
-	public static void e(String log) {
-		if (Constant.isDebug)
-			Log.e(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void e(String log) {
+        if (isDebug)
+            Log.e(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void e(String tag, String log) {
-		if (Constant.isDebug)
-			Log.e(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void e(String tag, String log) {
+        if (isDebug)
+            Log.e(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void v(String log) {
-		if (Constant.isDebug)
-			Log.v(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void v(String log) {
+        if (isDebug)
+            Log.v(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void v(String tag, String log) {
-		if (Constant.isDebug)
-			Log.v(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void v(String tag, String log) {
+        if (isDebug)
+            Log.v(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void d(String log) {
-		if (Constant.isDebug)
-			Log.d(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void d(String log) {
+        if (isDebug)
+            Log.d(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void d(String tag, String log) {
-		if (Constant.isDebug)
-			Log.d(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void d(String tag, String log) {
+        if (isDebug)
+            Log.d(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void i(String log) {
-		if (Constant.isDebug)
-			Log.i(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void i(String log) {
+        if (isDebug)
+            Log.i(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void i(String tag, String log) {
-		if (Constant.isDebug)
-			Log.i(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void i(String tag, String log) {
+        if (isDebug)
+            Log.i(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void w(String log) {
-		if (Constant.isDebug)
-			Log.w(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void w(String log) {
+        if (isDebug)
+            Log.w(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
-	public static void w(String tag, String log) {
-		if (Constant.isDebug)
-			Log.w(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
-	}
+    public static void w(String tag, String log) {
+        if (isDebug)
+            Log.w(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
 
 }
